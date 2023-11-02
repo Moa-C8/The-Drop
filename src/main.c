35b3cfc29590f9cde06d744e029f5f112695363c
Moa <creser08.wtf@gmail.com>
2023-11-02 18:03:57 +0100

    change for v1 delete erasePlayer and erase Wall Func

--- src/main.c
+++ src/main.c
@@ -14,14 +14,13 @@ void SDL_DestroyAndExit(SDL_Window *win,SDL_Renderer *ren,const char *errorMsg);
 
 void StartingGame(SDL_Renderer *ren, int *playerX, int *playerY);
 void limit_FPS(unsigned int limit);
+void eraseGamingField(SDL_Renderer *ren);
 
 void drawPlayer(SDL_Renderer *ren, int playerX, int playerY);
-void erasePlayer(SDL_Renderer *ren, int playerX, int playerY);
 void moveRight(SDL_Renderer *ren,int *playerX, int *playerY);
 void moveLeft(SDL_Renderer *ren,int *playerX, int *playerY);
 
 void drawWalls(SDL_Renderer *ren, int wallY, int wallH);
-void eraseWalls(SDL_Renderer *ren, int wallY, int wallH);
 void scrollWalls(SDL_Renderer *ren, int *wallY, int *wallH);
 
 void drawObstacle(SDL_Renderer *ren, int obstacleY);
@@ -89,15 +88,15 @@ int main(int argc, char** argv)
         limit_FPS(frame);
 
         if(playing == 1){
-            //SDL_RenderClear(ren);
-            eraseWalls(ren,*wy,*wh);
-            eraseObstacle(ren,*oy);
+            eraseGamingField(ren);
+            drawPlayer(ren,*px,*py);
             *wy -= 1;
             *wh += 1;
             *oy += 1;
             drawWalls(ren,*wy,*wh);
             drawObstacle(ren,*oy);
         }
+
         while(SDL_PollEvent(&event)){
             switch (event.type){
                 case SDL_KEYDOWN:
@@ -179,9 +178,6 @@ void SDL_DestroyAndExit(SDL_Window *win,SDL_Renderer *ren,const char *errorMsg) 
 
 
 void StartingGame(SDL_Renderer *ren, int *playerX, int *playerY){
-
-    erasePlayer(ren,*playerX,*playerY);
-
     *playerX = 40;
     drawPlayer(ren,*playerX,*playerY);
 
@@ -198,6 +194,23 @@ void limit_FPS(unsigned int limit){
     else{
         SDL_Delay(limit-ticks);
     }
+}
+
+void eraseGamingField(SDL_Renderer *ren){
+    if(SDL_SetRenderDrawColor(ren, 0,0,0, SDL_ALPHA_OPAQUE) != 0){
+        SDL_ExitWithError("change color");
+    }
+
+    SDL_Rect Field;
+    Field.x = 0;
+    Field.y = 0;
+    Field.h = HEIGHT;
+    Field.w = WIDTH;
+
+    if(SDL_RenderFillRect(ren, &Field) != 0){ 
+        SDL_ExitWithError("can't draw player");
+    }
+
 }
 
 
@@ -228,33 +241,6 @@ void drawWalls(SDL_Renderer *ren, int wallY, int wallH){
             }
 }
 
-void eraseWalls(SDL_Renderer *ren, int wallY, int wallH){
-    if(SDL_SetRenderDrawColor(ren, 0,0,0, SDL_ALPHA_OPAQUE) != 0){
-        SDL_ExitWithError("change color");
-    }
-    SDL_Rect leftWall;
-    SDL_Rect rightWall;
-
-    leftWall.x = 0;
-    rightWall.x = WIDTH-30;
-
-    leftWall.y = wallY;
-    rightWall.y = wallY;
-
-    leftWall.w = 30;
-    rightWall.w = 30;
-
-    leftWall.h = wallH;
-    rightWall.h = wallH;
-
-    if(SDL_RenderFillRect(ren, &leftWall) != 0){ 
-                SDL_ExitWithError("can't draw left wall");
-            }
-    if(SDL_RenderFillRect(ren, &rightWall) != 0){ 
-                SDL_ExitWithError("can't draw right wall");
-            }
-}
-
 void drawPlayer(SDL_Renderer *ren, int playerX, int playerY){
     if(SDL_SetRenderDrawColor(ren, 0,200,0, SDL_ALPHA_OPAQUE) != 0){
         SDL_ExitWithError("change color");
@@ -271,32 +257,12 @@ void drawPlayer(SDL_Renderer *ren, int playerX, int playerY){
     }
 }
 
-void erasePlayer(SDL_Renderer *ren, int playerX, int playerY){
-    if(SDL_SetRenderDrawColor(ren, 0,0,0, SDL_ALPHA_OPAQUE) != 0){
-        SDL_ExitWithError("change color");
-    }
-
-    SDL_Rect player;
-    player.y = playerY;
-    player.x = playerX;
-    player.w = PlayerWidth;
-    player.h = PlayerHeight;
-    
-    if(SDL_RenderFillRect(ren, &player) != 0){ 
-                SDL_ExitWithError("can't draw player");
-    }
-}
-
 void moveRight(SDL_Renderer *ren,int *playerX, int *playerY){
-    erasePlayer(ren,*playerX,*playerY);
-
     *playerX += 12;
     drawPlayer(ren,*playerX,*playerY);
 }
 
 void moveLeft(SDL_Renderer *ren,int *playerX, int *playerY){
-    erasePlayer(ren,*playerX,*playerY);
-
     *playerX -= 12;
     drawPlayer(ren,*playerX,*playerY);
 }
