e871f32fbf5b3a4fdda818779e5e95890bf80aad
Moa <creser08.wtf@gmail.com>
2023-11-02 18:16:46 +0100

    header Obstacles

---
+++ src/obstacles.h
@@ -0,0 +1,12 @@
+#include <SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef OBSTACLES_H
+#define OBSTACLES_H
+
+typedef struct {
+    SDL_Rect rects[2]; // Un tableau de 3 SDL_Rect pour chaque obstacle
+} Obstacle;
+
+#endif
